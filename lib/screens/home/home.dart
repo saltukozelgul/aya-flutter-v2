dadda228fb1cf57e835cbe0ef8409b7149c41224
saltukozelgul <saltukozelgul@gmail.com>
2023-05-21 12:48:29 +0300

    design changes

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:aya_flutter_v2/constants/colors.dart';
+import 'package:aya_flutter_v2/extensions/strings.dart';
 import 'package:aya_flutter_v2/screens/lists/lists.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -33,7 +34,7 @@ class _HomeState extends State<Home> {
                 : const Text('Anasayfa'),
         centerTitle: true,
         backgroundColor: AppColors.primary,
-        elevation: 0.0,
+        elevation: 5.0,
         actions: <Widget>[
           IconButton(
             onPressed: _auth.signOut,
@@ -75,8 +76,8 @@ class _HomeState extends State<Home> {
           setState(() => _currentIndex = index);
         },
         backgroundColor: AppColors.primary,
-        activeColor: Colors.white,
-        inactiveColor: Colors.white,
+        activeColor: AppColors.white,
+        inactiveColor: AppColors.white,
       ),
     );
   }
@@ -139,7 +140,7 @@ class _HomeState extends State<Home> {
                       //kategoriler listesi
                       for (var i = 0; i < _Strings.tagler.length; i++)
                         Tab(
-                          child: Text(_Strings.tagler[i],
+                          child: Text(_Strings.tagler[i].capitalizeFirstChar(),
                               style: Theme.of(context)
                                   .textTheme
                                   .bodyMedium
@@ -155,8 +156,14 @@ class _HomeState extends State<Home> {
                 physics: const NeverScrollableScrollPhysics(),
                 //ilanlar listesi
                 shrinkWrap: true,
-                itemBuilder: (context, index) => _Ilan(context, "Seda",
-                    "Battaniye lazim", "Merkez Ankara", 45.0, 48.0, index),
+                itemBuilder: (context, index) => _Ilan(
+                    context,
+                    "Seda",
+                    "Battaniye lazim",
+                    "Merkez Ankara",
+                    39.925533,
+                    32.866287,
+                    index),
                 itemCount: 5,
               ),
             ],
@@ -172,108 +179,126 @@ class _HomeState extends State<Home> {
   Container _Ilan(BuildContext context, String user, String desc, String loc,
           double lat, double lng, int index) =>
       Container(
-        height: 250,
+        height: 300,
         margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-        decoration: BoxDecoration(
+        child: Card(
           color: AppColors.white,
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              //ilan sahibi kısmı
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 5),
-              child: Text(user,
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      fontSize: 20,
-                      color: AppColors.disable,
-                      fontWeight: FontWeight.bold)),
-            ),
-            Padding(
-              //ilan açıklaması kısmı
-              padding: const EdgeInsets.only(left: 15.0, right: 10, top: 5),
-              child: Text(desc,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium
-                      ?.copyWith(fontSize: 18, color: AppColors.disable)),
-            ),
-            Padding(
-              //tag ve konum kısmı
-              padding: const EdgeInsets.only(left: 15.0, right: 15, top: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    height: 30,
-                    width: 150,
-                    child: ListView.builder(
-                      itemBuilder: (context, index) =>
-                          _ilanTag(context, _Strings.tagler[index]),
-                      itemCount: 5,
-                      scrollDirection: Axis.horizontal,
-                    ),
-                  ),
-                  Text(loc,
-                      textAlign: TextAlign.right,
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyMedium
-                          ?.copyWith(fontSize: 18, color: AppColors.disable)),
-                ],
-              ),
-            ),
-            Container(
-              //maps kısmı
-              height: 100,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20)),
-              ),
-              clipBehavior: Clip.hardEdge,
-              width: MediaQuery.of(context).size.width,
-              child: MapLayout(
-                controller: MapController(
-                  location: const LatLng(0, 0),
-                  zoom: 2,
-                ),
-                builder: (context, transformer) {
-                  return TileLayer(
-                    builder: (context, x, y, z) {
-                      final tilesInZoom = pow(2.0, z).floor();
-
-                      while (x < 0) {
-                        x += tilesInZoom;
-                      }
-                      while (y < 0) {
-                        y += tilesInZoom;
-                      }
-
-                      x %= tilesInZoom;
-                      y %= tilesInZoom;
-
-                      //Google Maps
-                      final url =
-                          'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
-
-                      return CachedNetworkImage(
-                        imageUrl: url,
-                        fit: BoxFit.cover,
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
+          elevation: 7,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _ilanUsername(user, context),
+              _ilanDesc(desc, context),
+              _ilanLocationAndTags(loc, context),
+              _ilanMap(context, lat, lng),
+            ],
+          ),
         ),
       );
+
+  Container _ilanMap(BuildContext context, double lat, double lng) {
+    return Container(
+      //maps kısmı
+      height: 150,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+      ),
+      clipBehavior: Clip.hardEdge,
+      width: MediaQuery.of(context).size.width,
+      child: MapLayout(
+        controller: MapController(
+          location: LatLng(lat, lng),
+          zoom: 14,
+        ),
+        builder: (context, transformer) {
+          return TileLayer(
+            builder: (context, x, y, z) {
+              final tilesInZoom = pow(2.0, z).floor();
+
+              while (x < 0) {
+                x += tilesInZoom;
+              }
+              while (y < 0) {
+                y += tilesInZoom;
+              }
+
+              x %= tilesInZoom;
+              y %= tilesInZoom;
+
+              //Google Maps
+              final url =
+                  'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
+
+              return CachedNetworkImage(
+                imageUrl: url,
+                fit: BoxFit.cover,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Padding _ilanLocationAndTags(String loc, BuildContext context) {
+    return Padding(
+      //tag ve konum kısmı
+      padding: const EdgeInsets.only(left: 15.0, right: 15, top: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 30,
+            width: 150,
+            child: ListView.builder(
+              itemBuilder: (context, index) =>
+                  _ilanTag(context, _Strings.tagler[index]),
+              itemCount: 2,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Text(loc,
+              textAlign: TextAlign.right,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium
+                  ?.copyWith(fontSize: 18, color: AppColors.disable)),
+        ],
+      ),
+    );
+  }
+
+  Padding _ilanDesc(String desc, BuildContext context) {
+    return Padding(
+      //ilan açıklaması kısmı
+      padding: const EdgeInsets.only(left: 15.0, right: 10, top: 5),
+      child: Text(desc,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context)
+              .textTheme
+              .bodyMedium
+              ?.copyWith(fontSize: 18, color: AppColors.disable)),
+    );
+  }
+
+  Padding _ilanUsername(String user, BuildContext context) {
+    return Padding(
+      //ilan sahibi kısmı
+      padding:
+          const EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 5),
+      child: Text(user,
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+              fontSize: 20,
+              color: AppColors.disable,
+              fontWeight: FontWeight.bold)),
+    );
+  }
 
   Container _ilanTag(BuildContext context, String index) => Container(
         //tagler için ayarlar
@@ -281,13 +306,13 @@ class _HomeState extends State<Home> {
           color: AppColors.primary,
           borderRadius: BorderRadius.circular(20),
         ),
-        padding: const EdgeInsets.all(5),
-        margin: const EdgeInsets.only(right: 5),
+        padding: const EdgeInsets.all(7),
+        margin: const EdgeInsets.only(right: 8),
         child: Text(index.toString(),
             style: Theme.of(context)
                 .textTheme
                 .bodyMedium
-                ?.copyWith(fontSize: 15, color: AppColors.disable)),
+                ?.copyWith(fontSize: 14, color: AppColors.white)),
       );
 }
 
